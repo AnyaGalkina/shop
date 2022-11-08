b1660a03792d59e3f52091b62f2a296ba211af79
AnyaGalkina <anna.blackbird1@gmail.com>
2022-11-08 19:30:14 +0300

    Add store and provider

---
+++ src/app/store.ts
@@ -0,0 +1,18 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+
+import { cartReducer, productsReducer } from '../features';
+
+const rootReducer = combineReducers({
+    productsPage: productsReducer,
+    cartPage: cartReducer,
+});
+
+export const store = configureStore({
+    reducer: rootReducer,
+    middleware: getDefaultMiddleware => getDefaultMiddleware().prepend(thunk),
+    devTools: true,
+});
+
+export type AppStateType = ReturnType<typeof rootReducer>;
--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,19 @@ import React from 'react';
 
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
 
-import App from './App';
+import { store, App } from './app';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
     <HashRouter>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </HashRouter>,
 );
 
