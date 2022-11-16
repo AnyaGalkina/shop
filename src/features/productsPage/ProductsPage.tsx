413cf6bb598d0f8e37b501bac5092061d4578c0c
AnyaGalkina <anna.blackbird1@gmail.com>
2022-11-17 01:42:41 +0300

    Add useffect eith get request for products

--- src/features/productsPage/ProductsPage.tsx
+++ src/features/productsPage/ProductsPage.tsx
@@ -1,26 +1,24 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useEffect } from 'react';
 
 import { useSelector } from 'react-redux';
 
 import { AppStateType } from '../../app/store';
+import { useAppDispatch } from '../../common';
 
 import { ProductItem } from './productItem';
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { getProductsTC, ProductType } from './products-reducer';
 import styles from './ProductsPage.module.css';
 
-// import { useAppDispatch } from '../../common';
-
 export const ProductsPage = (): ReactElement => {
-    // const dispatch = useAppDispatch();
+    const dispatch = useAppDispatch();
     const products = useSelector<AppStateType, Array<ProductType>>(
         state => state.productsPage.products,
     );
     // console.log(JSON.stringify(products));
 
-    // useEffect(() => {
-    //     dispatch(getProductsTC({}));
-    // }, []);
+    useEffect(() => {
+        dispatch(getProductsTC({}));
+    }, [dispatch]);
 
     return (
         <div className={styles.productsPageContainer}>
--- src/features/productsPage/products-reducer.ts
+++ src/features/productsPage/products-reducer.ts
@@ -1,40 +1,41 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-import cake1 from '../../common/assets/image/products/cake1.png';
-import cake2 from '../../common/assets/image/products/cake2.png';
-import cake3 from '../../common/assets/image/products/cake3.png';
-import cake4 from '../../common/assets/image/products/cake4.png';
-import cake5 from '../../common/assets/image/products/cake5.jpg';
-import cake7 from '../../common/assets/image/products/cake7.jpg';
+import { productsPageAPI } from './api-productsPage';
+
+// import cake1 from '../../common/assets/image/products/cake1.png';
+// import cake2 from '../../common/assets/image/products/cake2.png';
+// import cake3 from '../../common/assets/image/products/cake3.png';
+// import cake4 from '../../common/assets/image/products/cake4.png';
+// import cake5 from '../../common/assets/image/products/cake5.jpg';
+// import cake7 from '../../common/assets/image/products/cake7.jpg';
 
 // import { productsPageAPI, productsPageAPI } from './api-productsPage';
 
-const imgSrcs = [cake1, cake2, cake3, cake4, cake5, cake7];
-// eslint-disable-next-line
-const pricePerUnits = [30, 60, 40, 35, 55, 120];
-const productNames = [
-    'BERRY PIE',
-    'CREAM CAKE',
-    'FRUIT BOWL',
-    'CHOCO ROLLS',
-    'FRUIT CAKE',
-    'ASSORTMENT CAKES',
-];
+// const imgSrcs = [cake1, cake2, cake3, cake4, cake5, cake7];
+// // eslint-disable-next-line
+// const pricePerUnits = [30, 60, 40, 35, 55, 120];
+// const productNames = [
+//     'BERRY PIE',
+//     'CREAM CAKE',
+//     'FRUIT BOWL',
+//     'CHOCO ROLLS',
+//     'FRUIT CAKE',
+//     'ASSORTMENT CAKES',
+// ];
 
-// eslint-disable-next-line
-const productsArray: ProductType[] = new Array(6).fill(null).map((_, index) => ({
-    productId: index.toString(),
-    imgSrc: imgSrcs[index],
-    pricePerUnit: pricePerUnits[index],
-    productName: productNames[index],
-    productDescription:
-        'Sed non mauris vitae erat consequat auctor. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin nec sagitis sem nibh id elit vulputate cursus a sit amet mauris.',
-}));
+// // eslint-disable-next-line
+// const productsArray: ProductType[] = new Array(6).fill(null).map((_, index) => ({
+//     productId: index.toString(),
+//     imgSrc: imgSrcs[index],
+//     pricePerUnit: pricePerUnits[index],
+//     productName: productNames[index],
+//     productDescription:
+//         'Sed non mauris vitae erat consequat auctor. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin nec sagitis sem nibh id elit vulputate cursus a sit amet mauris.',
+// }));
 
 export const initialState = {
-    // products: [] as Array<ProductType>,
-    products: [...productsArray] as Array<ProductType>,
+    products: [] as Array<ProductType>,
+    // products: [...productsArray] as Array<ProductType>,
 };
 
 export type ProductType = {
@@ -45,23 +46,23 @@ export type ProductType = {
     pricePerUnit: number;
 };
 
-// export const getProductsTC = createAsyncThunk(
-//     'productsPage/setProducts',
-//     async (param: {}, { dispatch }) => {
-//         //     // dispatch(setAppStatus({status: "loading"}))
-//         try {
-//             const response = await productsPageAPI.getProducts();
-//
-//             // console.log(response.data.products);
-//             dispatch(setProducts({ products: response.data.products }));
-//         } catch (error) {
-//             //         // handleServerNetworkError(error, dispatch);
-//             //         return rejectWithValue(null);
-//         } finally {
-//             //         // dispatch(setAppStatus({status: "idle"}));
-//         }
-//     },
-// );
+export const getProductsTC = createAsyncThunk(
+    'productsPage/setProducts',
+    async (param: {}, { dispatch }) => {
+        //     // dispatch(setAppStatus({status: "loading"}))
+        try {
+            const response = await productsPageAPI.getProducts();
+
+            // console.log(response.data.products);
+            dispatch(setProducts({ products: response.data.products }));
+        } catch (error) {
+            //         // handleServerNetworkError(error, dispatch);
+            //         return rejectWithValue(null);
+        } finally {
+            //         // dispatch(setAppStatus({status: "idle"}));
+        }
+    },
+);
 
 const slice = createSlice({
     name: 'productsPage',
