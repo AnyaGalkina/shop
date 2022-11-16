3aadc3cbd193eecb18a2bc0c362eed34b8308142
AnyaGalkina <anna.blackbird1@gmail.com>
2022-11-16 14:19:26 +0300

    Update 404 page, add link to main page

--- src/common/components/pageNotFound/PageNotFound.tsx
+++ src/common/components/pageNotFound/PageNotFound.tsx
@@ -1,3 +1,32 @@
 import React, { ReactElement } from 'react';
 
-export const PageNotFound = (): ReactElement => <div>404</div>;
+import { Button } from '@material-ui/core';
+import { useNavigate } from 'react-router-dom';
+
+import iceNotFoundPage from '../../assets/image/notFoundPage.png';
+import { ROUTES } from '../../enums';
+
+import styles from './PageNotFound.module.css';
+
+export const PageNotFound = (): ReactElement => {
+    const navigate = useNavigate();
+
+    const onShoppingClickHandler = (): void => {
+        navigate(ROUTES.PRODUCT_PAGE);
+    };
+
+    return (
+        <div>
+            <img className={styles.img} alt="404" src={iceNotFoundPage} />
+            <div className={styles.button}>
+                <Button
+                    style={{ color: '#fff' }}
+                    variant="contained"
+                    onClick={onShoppingClickHandler}
+                >
+                    Go to shopping
+                </Button>
+            </div>
+        </div>
+    );
+};
