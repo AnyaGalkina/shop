293099fa889e4fd1223122243f4633f7013324d8
Anna Galkina <anna.blackbird1@gmail.com>
2023-01-04 18:52:57 +0700

    Refactoring

--- src/features/cartPage/contactDetails/errorMessage/ErrorMessage.tsx
+++ src/features/cartPage/contactDetails/errorMessage/ErrorMessage.tsx
@@ -8,9 +8,7 @@ type PropsType = {
 };
 
 export const ErrorMessage = ({ errorMessage, isTouched }: PropsType): ReactElement => {
-    return (
-        <div className={styles.error}>
-            {isTouched && errorMessage ? errorMessage : ''}
-        </div>
-    );
+    const showErrorMessage = isTouched && errorMessage;
+
+    return <div className={styles.error}>{showErrorMessage ? errorMessage : ''}</div>;
 };
